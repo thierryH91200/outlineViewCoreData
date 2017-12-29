526d6c15a3953f66bf7b396c36f97d5f29a8cdef
thierryH91200 <thierry.hentic@gmail.com>
2017-12-29 12:08:03 +0100

    init

--- outlineViewCoreData/MainWindowController.swift
+++ outlineViewCoreData/MainWindowController.swift
@@ -14,11 +14,8 @@ class MainWindowController: NSWindowController , NSOutlineViewDataSource {
     @IBOutlet var arrayController: NSArrayController!
     @IBOutlet weak var myOutlineView: NSOutlineView!
     
-    // sDWF6OfQ_K9PTky4lAratXxj6im4_szvQOpNBRCGsMALoVbwwd
     var dragType = [NSPasteboard.PasteboardType]()
     var draggedNode:Any? = nil
-    
-    var data = [Group]()
     
     @objc  var managedObjectContext: NSManagedObjectContext = (NSApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -33,9 +30,7 @@ class MainWindowController: NSWindowController , NSOutlineViewDataSource {
         notification.contentImage = NSImage(named: NSImage.Name("icon_Upload-Information-icon_24x24"))
         NSUserNotificationCenter.default.deliver(notification)
         
-        data = getAllEntity()
-        
-        dragType = [NSPasteboard.PasteboardType(rawValue: "factorialDragType")]
+        dragType = [NSPasteboard.PasteboardType(rawValue: "DragType")]
         myOutlineView.registerForDraggedTypes(dragType)
     }
     
@@ -43,47 +38,6 @@ class MainWindowController: NSWindowController , NSOutlineViewDataSource {
         let theArrObjFromArrCtrl = arrayController.arrangedObjects as? [Group]
         for theGrp in theArrObjFromArrCtrl! {
             print("\(String(describing: theGrp.name))")
-        }
-    }
-    
-    func getAllEntity() -> [Group] {
-        
-        do {
-            let fetchRequest = NSFetchRequest<Group>(entityName: "Group")
-            //            let predicate = NSPredicate(format: "compte == %@", compteCourant!)
-            //            fetchRequest.predicate = predicate
-            
-            data = try managedObjectContext.fetch(fetchRequest)
-        }catch {
-            print("Error fetching data from CoreData")
-        }
-        for d in data {
-            print(d.name!)
-            print(d.parent ?? "nil")
-            print(d.subGroups ?? "nil")
-            print()
-
-
-
-        }
-        return data
-    }
-    
-    
-    func addData() {
-        let root: [String : Any] = [ "name": "Max Mustermann", "isLeaf": false ]
-        let dict = NSMutableDictionary(dictionary: root)
-        dict.setObject([Group()], forKey: "children" as NSCopying)
-        
-        treeController.addObject(dict)
-    }
-    
-    
-    @objc  func isHeader(item: AnyObject) -> Bool {
-        if let item = item as? NSTreeNode {
-            return !(item.representedObject is Group)
-        } else {
-            return !(item is Group)
         }
     }
     
@@ -142,30 +96,29 @@ class MainWindowController: NSWindowController , NSOutlineViewDataSource {
         return false
     }
     
-func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo, proposedItem item: Any?, proposedChildIndex index: Int) -> NSDragOperation {
-    
-    var retVal:NSDragOperation = NSDragOperation()
-    
-    // drags to the root are always acceptable
-    let item1 = item as? NSTreeNode
-    if item1?.representedObject == nil {
-        return .generic
-    }
+    func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo, proposedItem item: Any?, proposedChildIndex index: Int) -> NSDragOperation {
+        
+        let retVal = NSDragOperation()
+        
+        // drags to the root are always acceptable
+        let item1 = item as? NSTreeNode
+        if item1?.representedObject == nil {
+            return .generic
+        }
         // Verify that we are not dragging a parent to one of it's ancestors
         // causes a parent loop where a group of nodes point to each other
         // and disappear from the control
-    
-    let draggedNode1 = draggedNode as? NSTreeNode
-    let dragged = draggedNode1?.representedObject as! NSManagedObject
-    
-    let newP = item1?.representedObject as! NSManagedObject
-    if category(dragged, isSubCategoryOf: newP) {
         
-        return retVal
+        let draggedNode1 = draggedNode as? NSTreeNode
+        let dragged = draggedNode1?.representedObject as! NSManagedObject
+        
+        let newP = item1?.representedObject as! NSManagedObject
+        if category(dragged, isSubCategoryOf: newP) {
+            
+            return retVal
+        }
+        return .generic
     }
-    retVal = .generic
-    return retVal
-}
     
     func outlineView(_ outlineView: NSOutlineView, draggingSession session: NSDraggingSession, endedAt screenPoint: NSPoint, operation: NSDragOperation) {
         debugPrint("Drag session ended")
@@ -174,10 +127,7 @@ func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo
     
     func outlineView(_ outlineView: NSOutlineView, draggingSession session: NSDraggingSession, willBeginAt screenPoint: NSPoint, forItems draggedItems: [Any]) {
         draggedNode = draggedItems[0] as AnyObject?
-        session.draggingPasteboard.setData(Data(), forType: NSPasteboard.PasteboardType(rawValue: "factorialDragType"))
+        session.draggingPasteboard.setData(Data(), forType: NSPasteboard.PasteboardType(rawValue: "DragType"))
         debugPrint("Drag session begin")
-        
     }
-
-
 }
